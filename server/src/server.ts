dca06b4ef8164bfdc9ebbed505919a92fbe4ac6e
Pedro Santos <142705026+pedro-santos21@users.noreply.github.com>
2023-09-25 03:49:35 -0300

    migrated server.ts code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,40 @@
 // Imports
-const express = require('express')
-require('dotenv').config()
-
-// Codebase imports
-import { startMongo } from "./config/database"
+import express, {Request, Response} from "express";
+const router = express.Router();
+require('dotenv').config();
+import cors from "cors";
+const bodyParser = require("body-parser");
+const helmet = require('helmet');
+const path = require('path');
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000;
 
+// Codebase imports
+import { startMongo, printMongooseState } from "./config/database";
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Security
+app.disable('x-powered-by') // Reduce fingerpinting
+
+// Database
+startMongo().then(() => {
+  printMongooseState();
+});
+
+
+// Routes
 app.get('/', (req:any, res:any) => {
   res.send('Hello World!')
 })
 
+// Start server
 app.listen(port, () => {
-  console.log(`\nServer listening on ${port}\n`)
-})
+  console.info(`Backend server is listening on http://localhost:${port}`);
+});
